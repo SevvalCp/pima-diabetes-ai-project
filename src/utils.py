ef75ed13b0c47dd2bd85e4f4d48a6482829ee379
SevvalCp <capoglu.sevval@gmail.com>
2026-06-10 16:58:57 +0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,10 @@
-
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+cm_lr=confusion_matrix(y_test,y_pred_lr)
+plt.figure(figsize=(6,4))
+sns.heatmap(cm_lr,annot=True,fmt='d',cmap='Blues')
+plt.title("Confusion Matrix - Logistic Regression")
+plt.xlabel("Predicted")
+plt.ylabel("Actual")
+plt.show()
